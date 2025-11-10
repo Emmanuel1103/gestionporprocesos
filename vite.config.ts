216485d928d8e623870a229e7f925c757a614a31
Emmanuel Morales <emorales14@cuc.edu.co>
2025-11-09 22:14:12 -0500

    Fix: added base path for production3

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: "/gestionporprocesos/",
+  base: mode === "development" ? "/" : "/gestionporprocesos/", // 👈 esto es la clave
   server: {
     host: "::",
     port: 8080,
